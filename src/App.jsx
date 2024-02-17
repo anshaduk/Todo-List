a6080afc55865baba8ab94fdd8906451c35ba354
Anshad UK <anshadu@gmail.com>
2024-02-17 21:13:36 +0530

    This is my todo-List using useState

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import TodoWrapper from './Components/TodoWrapper'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <TodoWrapper />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
