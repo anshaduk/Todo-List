d8f1f1d5a18a6507843426681eecb7d03e12c0c6
Anshad UK <anshadu@gmail.com>
2024-02-21 23:24:29 +0530

    updated

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -4,11 +4,17 @@ const TodoForm = ({addTodo}) => {
     const[value,setValue]=useState('')
     const handleSubmit=(e)=>{
         e.preventDefault();
-        addTodo(value);
-        setValue("")
+        if(/^\s*$/.test(value)){
+          alert('Enter a Todo')
+          setValue("")
+        }else{
+          addTodo(value);
+          setValue("")
+        }
+        
     }
   return (
-    <form className='TodoForm' onSubmit={handleSubmit}>
+    <form  className='TodoForm' onSubmit={handleSubmit}>
         <input type="text"  className='todo-input' value={value} placeholder='What is the task today..?'   onChange={(e)=>setValue(e.target.value)}/>
         <button type="submit" className='todo-btn'>Add Task</button>
     </form>
